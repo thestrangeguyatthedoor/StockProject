492ac906b59573766bf0ea884a6775cb7a29ac93
thestrangeguyatthedoor <sstroppy93@gmail.com>
2021-07-02 19:19:55 +0200

    stock_finder.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from yahoo-finance import Shares
+
+def Main():
+    def Buy():
+        pass
+    def Sell():
+        pass
+    def Result():
+        pass
---
+++ stock_finder.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from yahoo-finance import Shares
+
+df_r = pd.read_csv("data.csv")
+df_w = pd.to_csv()
